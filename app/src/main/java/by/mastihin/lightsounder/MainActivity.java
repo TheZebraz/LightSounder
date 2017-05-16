6b1ac34b19c2f61110552eae1484067df5ee9049
thezebraz <andrew.evtukhov@gmail.com>
2017-05-16 15:19:43 +0300

    Animation added

--- app/src/main/java/by/mastihin/lightsounder/MainActivity.java
+++ app/src/main/java/by/mastihin/lightsounder/MainActivity.java
@@ -1,74 +1,174 @@
 package by.mastihin.lightsounder;
 
 import android.app.Activity;
+import android.graphics.drawable.AnimationDrawable;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.media.AudioFormat;
 import android.media.AudioManager;
 import android.media.AudioTrack;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity implements OnClickListener {
-    Button startSound;
-    Button endSound;
 
-    AudioSynthesisTask audioSynth;
-    boolean keepGoing = false;
-    float synth_frequency = 440; // 440 Hz, Middle A
+    private Button mButtonStartSound;
+    private TextView mTextViewFrequency;
+    private TextView mTextViewLightLevel;
+    private View mValues;
+    private SeekBar mSeekBarStartFrequencyCoef;
+
+    private double mStartFrequencyCoef = 50;
+    private boolean mKeepGoing = false;
+    private double mSynthFrequency = 440;
+
+    private SensorManager mSensorManager;
+    private Sensor mLightSensor;
+
+    private Thread mSoundThread = new Thread(new Runnable() {
+        @Override
+        public void run() {
+            final int SAMPLE_RATE = 44100;
+            int minSize = AudioTrack.getMinBufferSize(SAMPLE_RATE, AudioFormat.CHANNEL_IN_STEREO, AudioFormat.ENCODING_PCM_16BIT);
+            AudioTrack audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC, SAMPLE_RATE, AudioFormat.CHANNEL_IN_STEREO, AudioFormat.ENCODING_PCM_16BIT, minSize, AudioTrack.MODE_STREAM);
+            audioTrack.play();
+            short[] buffer = new short[minSize];
+            float angle = 0;
+            while (mKeepGoing) {
+                for (int i = 0; i < buffer.length; i++) {
+                    buffer[i] = (short) (Short.MAX_VALUE * ((float) Math.sin(angle)));
+                    angle += (float) (Math.PI) * mSynthFrequency / SAMPLE_RATE;
+                }
+                audioTrack.write(buffer, 0, buffer.length);
+            }
+        }
+    });
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        startSound = (Button) this.findViewById(R.id.start_sound);
-        startSound.setOnClickListener(this);
-        endSound = (Button) this.findViewById(R.id.end_sound);
-        endSound.setOnClickListener(this);
-        endSound.setEnabled(false);
+
+        initBackground();
+        initViews();
+        initSensor();
+        initSeek();
+    }
+
+    private void initViews() {
+        mButtonStartSound = (Button) this.findViewById(R.id.start_sound);
+        mButtonStartSound.setOnClickListener(this);
+
+        mValues = findViewById(R.id.values);
+        mValues.setVisibility(View.INVISIBLE);
+
+        mTextViewFrequency = (TextView) findViewById(R.id.tvFreqValue);
+        mTextViewLightLevel = (TextView) findViewById(R.id.tvLightValue);
+    }
+
+    private void initBackground() {
+        RelativeLayout constraintLayout = (RelativeLayout) findViewById(R.id.activity_main);
+
+        AnimationDrawable animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(2500);
+        animationDrawable.setExitFadeDuration(5000);
+        animationDrawable.start();
+    }
+
+    private void initSeek() {
+        mSeekBarStartFrequencyCoef = (SeekBar) findViewById(R.id.sbStartFrequency);
+        mSeekBarStartFrequencyCoef.setMax(500);
+        mSeekBarStartFrequencyCoef.setOnSeekBarChangeListener(mOnSeekBarChangeListener);
+    }
+
+    SeekBar.OnSeekBarChangeListener mOnSeekBarChangeListener = new SeekBar.OnSeekBarChangeListener() {
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+            double value = seekBar.getProgress();
+            switch (seekBar.getId()) {
+                case R.id.sbStartFrequency:
+                    mStartFrequencyCoef = value;
+                    break;
+            }
+        }
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
+        }
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+        }
+    };
+
+    private void initSensor() {
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        mLightSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
     }
 
     @Override
     public void onPause() {
         super.onPause();
-        keepGoing = false;
-        endSound.setEnabled(false);
-        startSound.setEnabled(true);
+        mKeepGoing = false;
     }
 
     public void onClick(View v) {
-        if (v == startSound) {
-            keepGoing = true;
-            audioSynth = new AudioSynthesisTask();
-            audioSynth.execute();
-            endSound.setEnabled(true);
-            startSound.setEnabled(false);
-        } else if (v == endSound) {
-            keepGoing = false;
-            endSound.setEnabled(false);
-            startSound.setEnabled(true);
+        if (!mKeepGoing) {
+            start();
+        } else {
+            stop();
         }
     }
 
-    private class AudioSynthesisTask extends AsyncTask {
-        @Override
-        protected Object doInBackground(Object[] objects) {
-            final int SAMPLE_RATE = 11025;
-            int minSize = AudioTrack.getMinBufferSize(SAMPLE_RATE, AudioFormat.CHANNEL_CONFIGURATION_MONO, AudioFormat.ENCODING_PCM_16BIT);
-            AudioTrack audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC, SAMPLE_RATE, AudioFormat.CHANNEL_CONFIGURATION_MONO, AudioFormat.ENCODING_PCM_16BIT, minSize, AudioTrack.MODE_STREAM);
-            audioTrack.play();
-            short[] buffer = new short[minSize];
-            float angular_frequency = (float) (2 * Math.PI) * synth_frequency / SAMPLE_RATE;
-            float angle = 0;
-            while (keepGoing) {
-                for (int i = 0; i < buffer.length; i++) {
-                    buffer[i] = (short) (Short.MAX_VALUE * ((float) Math.sin(angle)));
-                    angle += angular_frequency;
-                }
-                audioTrack.write(buffer, 0, buffer.length);
-            }
-            return null;
+    private void stop() {
+        mKeepGoing = false;
+        mValues.setVisibility(View.INVISIBLE);
+        mButtonStartSound.setText("Start");
+
+        if (mSensorManager != null) {
+            mSensorManager.unregisterListener(mLightSensorListener);
+        }
+
+    }
+
+    private void start() {
+        mKeepGoing = true;
+        mButtonStartSound.setText("Stop");
+        mValues.setVisibility(View.VISIBLE);
+
+        if (mLightSensor != null) {
+            mSensorManager.registerListener(
+                    mLightSensorListener,
+                    mLightSensor,
+                    SensorManager.SENSOR_DELAY_FASTEST);
+        } else {
+            Toast.makeText(this, getString(R.string.all_no_sensor_message), Toast.LENGTH_SHORT).show();
+        }
+        if(!mSoundThread.isInterrupted()) {
+            mSoundThread.start();
         }
     }
+
+    private final SensorEventListener mLightSensorListener = new SensorEventListener() {
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        }
+
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            if (event.sensor.getType() == Sensor.TYPE_LIGHT) {
+                mSynthFrequency = event.values[0] + mStartFrequencyCoef;
+                mTextViewFrequency.setText(Double.toString(mSynthFrequency) + " Hz");
+                mTextViewLightLevel.setText(Double.toString(event.values[0]));
+            }
+        }
+    };
 }
